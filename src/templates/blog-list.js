b31da45b97f04f42f230cf2cfdb7ecf517ad915d
joshi-aparna <joshi-aparna@users.noreply.github.com>
2021-07-18 19:22:53 +0530

    Ikigai (#3)
    * ikigai

--- src/templates/blog-list.js
+++ src/templates/blog-list.js
@@ -19,7 +19,7 @@ class BlogIndex extends React.Component {
       <DefaultLayout>
         <SEO
           title={siteTitle}
-          keywords={[`blog`, `gatsby`, `javascript`, `react`]}
+          keywords={[`blog`, `gatsby`, `javascript`, `react`, `aparna ravindra`]}
         />
         <div className="content-box clearfix">
           {posts.map(({ node }) => {
