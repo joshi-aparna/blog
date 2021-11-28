84c7febd0729709fd86fec792dc119bb50c3107a
joshi-aparna <joshi-aparna@users.noreply.github.com>
2021-11-28 19:50:30 +0530

    Introducing analytics (#25)

--- gatsby-config.js
+++ gatsby-config.js
@@ -84,6 +84,24 @@ module.exports = {
             },
           },
           `gatsby-remark-prismjs`,
+          {
+            resolve: `gatsby-plugin-google-gtag`,
+            options: {
+                    // You can add multiple tracking ids and a pageview event will be fired for all of them.
+                    trackingIds: [
+                      "G-ZTX2MT1Q88", // Google Analytics / GA
+                    ],                    
+                    // This object is used for configuration specific to this plugin
+                    pluginConfig: {
+                      // Puts tracking script in the head instead of the body
+                      head: false,
+                      // Setting this parameter is also optional
+                      respectDNT: true,
+                      // Avoids sending pageview hits from custom paths
+                      exclude: ["/preview/**", ],
+                    },
+            },
+          },
         ],
       },
     },
