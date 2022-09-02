09a31caba9228456e3228e2ac71189aee8fddd98
joshi-aparna <joshi-aparna@users.noreply.github.com>
2022-09-02 23:15:26 +0530

    Update blog-list.js

--- src/templates/blog-list.js
+++ src/templates/blog-list.js
@@ -14,7 +14,7 @@ class BlogIndex extends React.Component {
     const isFirst = currentPage === 1
     const isLast = currentPage === numPages
     const prevPage = currentPage - 1 === 1 ? '/' : "../" + (currentPage - 1).toString()
-    const nextPage = "../" + (currentPage + 1).toString()
+    const nextPage = (currentPage === 1 ? "" : "../") + (currentPage + 1).toString()
 
     return (
       <DefaultLayout>
